6ed2cb0c111f3567854e95ac70860eb845b0ce63
wahid1099 <wahidahmed890@gmail.com>
2023-02-09 21:07:44 +0600

    adding some

--- todo/settings.py
+++ todo/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -51,7 +53,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'todo.urls'
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
