501045a873cb7ff8072cfe88784bb9d0555e867b
wahid1099 <wahidahmed890@gmail.com>
2023-02-10 10:03:44 +0600

    a

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo " BUILD START"
-Python3.10.7 -m pip install -r requirements.txt
-Python3.10.7 manage.py collectstatic --noinput --clear
+Python-3.10.7 -m pip install -r requirements.txt
+Python-3.10.7 manage.py collectstatic --noinput --clear
 echo " BUILD END" 
